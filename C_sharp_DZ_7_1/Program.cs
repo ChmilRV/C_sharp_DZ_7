5eafd504360209163f7fffc6d548b91c7129f03e
chmilrv@gmail.com <chmilrv@gmail.com>
2020-07-20 12:10:10 +0300

    Добавьте файлы проекта.

---
+++ C_sharp_DZ_7_1/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+/**/
+namespace C_sharp_DZ_7_1
+{
+
+
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+
+
+
+        }
+    }
+}
